9fa4b8c7ff7f472c5f19d0ca980f8260d466e002
Renan de Moraes <renan.de.moraes777@gmail.com>
2024-09-27 14:37:45 -0300

    configurando corretamente o teste

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,21 @@
 from app.main import verifica_idades
 import pandas as pd
+from pandas.testing import assert_frame_equal
 
 def test_verifica_idades():
-    df = pd.DataFrame()
-    df['Idade'] = [18, 22]
-    df['Verificacao Maior'] = ['Maior', 'Maior']
-    assert verifica_idades(df)
+    # DataFrame de exemplo
+    data = {'Nome': ['Alice', 'Bob', 'Charlie', 'David'],
+            'Idade': [15, 22, 17, 30]}
+    df = pd.DataFrame(data)
     
+    # DataFrame esperado após a verificação de idades
+    expected_data = {'Nome': ['Alice', 'Bob', 'Charlie', 'David'],
+                     'Idade': [15, 22, 17, 30],
+                     'Verificacao Maior': ['Menor', 'Maior', 'Menor', 'Maior']}
+    expected_df = pd.DataFrame(expected_data)
+
+    # Executa a função que será testada
+    result_df = verifica_idades(df)
+
+    # Verifica se o resultado é igual ao DataFrame esperado
+    assert_frame_equal(result_df, expected_df)
